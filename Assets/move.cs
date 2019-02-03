c7d3a5a88a82fdc8ff159699a3daaccaae492b0d
Maksatai <erkbvna@gmail.com>
2019-02-03 22:57:11 +0600

    Initial commit

---
+++ Assets/move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public GameObject player;
+
+    public float sidewaysForce = 1f;
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, sidewaysForce * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -sidewaysForce * Time.deltaTime));
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(0,90,0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -90, 0);
+        }
+
+    }
+}
